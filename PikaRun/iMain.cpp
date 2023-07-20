3b1657ce67845db15846d4b31fee894e51b7eb1d
BlasterOverlord <72990637+BlasterOverlord@users.noreply.github.com>
2023-07-20 21:50:30 +0600

    v1.2

--- PikaRun/iMain.cpp
+++ PikaRun/iMain.cpp
@@ -7,75 +7,51 @@
 #define button_width 405
 #define button_height 120
 #define gamestate_limit 3
+#define max_jump_limit 400
 
+// Global variables
 int mposx = 200, mposy = 173, dx = 5, dy = 5;
+int exitz = -1, hover = -1, gamestate = -1;
+int pikachuRunIndex = 0, pikachuJumpIndex = 0;
+int pikachuJumpCoordinate = 0;
 bool musicOn = true;
-int exitz = -1, hover = -1;
-int gamestate = -1;
+bool jumping = false, jumpingUp = false;
 
 struct buttonCoordinate {
     int x, y;
 } bCoordinate[3], exitButton;
 
+// Strings containing paths to files
 char button[3][30] = { "images\\buttons\\play1.bmp", "images\\buttons\\score.bmp", "images\\buttons\\ins.bmp"};
 char button2[3][30] = {"","",""};
 char homemenu[30] = "images\\menu\\-1.bmp";
-char play[30] = "images\\menu\\40.bmp";
 char score[30] = "images\\menu\\41.bmp";
 char ins[30] = "images\\menu\\42.bmp";
 char exitButtonImg[30] = "images\\buttons\\exit.bmp";
+char pikachuRun[4][30] = {"images\\pikachu\\run1.bmp", "images\\pikachu\\run2.bmp", "images\\pikachu\\run3.bmp", "images\\pikachu\\run4.bmp"};
+char pikachuJump[5][30] = {"images\\pikachu\\jump1.bmp", "images\\pikachu\\jump2.bmp", "images\\pikachu\\jump3.bmp", "images\\pikachu\\jump4.bmp", "images\\pikachu\\jump5.bmp"};
 
-void change() {
-    mposx += dx;
-    mposy += dy;
-
-    if (mposx >= width || mposx <= 0) 
-	{
-        dx *= (-1);
-    }
-    if (mposy >= height || mposy <= 0) 
-	{
-        dy *= (-1);
-    }
-}
+// function definitions
+void draw_homemenu();
+void draw_movement();
 
 void iDraw() {
     
 	iClear();
     if (gamestate == -1) 
 	{
-        iShowBMP(0, 0, homemenu);
-
-        for (int i = 0; i < gamestate_limit; i++) 
-		{
-            if (mposx >= bCoordinate[i].x && mposx <= bCoordinate[i].x + button_width &&
-                mposy >= bCoordinate[i].y && mposy <= bCoordinate[i].y + button_height) 
-			{
-                iShowBMP2(bCoordinate[i].x, bCoordinate[i].y, button[i], 255); // Hover effect
-            } 
-			else 
-			{
-                iShowBMP2(bCoordinate[i].x, bCoordinate[i].y, button[i], NULL);
-            }
-        }
-
-        if (mposx >= exitButton.x && mposx <= exitButton.x + button_width &&
-            mposy >= exitButton.y && mposy <= exitButton.y + button_height) {
-            iShowBMP2(exitButton.x, exitButton.y, exitButtonImg, 255); // Hover effect
-        }
-		else 
-		{
-            iShowBMP2(exitButton.x, exitButton.y, exitButtonImg, NULL);
-        }
+        draw_homemenu();
     }
-    else if (gamestate == 0) {
-        iShowBMP(0, 0, play);
-
+    else if (gamestate == 0) 
+	{
+		draw_movement();
     }
-    else if (gamestate == 1) {
+    else if (gamestate == 1) 
+	{
         iShowBMP(0, 0, score);
     }
-    else if (gamestate == 2) {
+    else if (gamestate == 2) 
+	{
         iShowBMP(0, 0, ins);
     }
 }
@@ -150,6 +126,14 @@ void iKeyboard(unsigned char key) {
     if (key == 'm') {
         musicOn = false;
     }
+	if (key == ' ')
+	{
+		if (!jumping)
+		{
+			jumping = true;
+			jumpingUp = true;
+		}
+	}
     
 	/*else {
         musicOn = true;
@@ -169,8 +153,83 @@ void iSpecialKeyboard(unsigned char key) {
     }
 }
 
-int main() {
-    
+void draw_homemenu() {
+	
+	iShowBMP(0, 0, homemenu);
+    for (int i = 0; i < gamestate_limit; i++) 
+	{
+		if (mposx >= bCoordinate[i].x && mposx <= bCoordinate[i].x + button_width &&
+            mposy >= bCoordinate[i].y && mposy <= bCoordinate[i].y + button_height) 
+		{
+            iShowBMP2(bCoordinate[i].x, bCoordinate[i].y, button[i], 255); // Hover effect
+        } 
+		else 
+		{
+            iShowBMP2(bCoordinate[i].x, bCoordinate[i].y, button[i], NULL);
+        }
+    }
+
+    if (mposx >= exitButton.x && mposx <= exitButton.x + button_width &&
+            mposy >= exitButton.y && mposy <= exitButton.y + button_height) 
+	{
+        iShowBMP2(exitButton.x, exitButton.y, exitButtonImg, 255); // Hover effect
+    }
+	else 
+	{
+        iShowBMP2(exitButton.x, exitButton.y, exitButtonImg, NULL);
+    }
+}
+
+void index() {
+
+	if (gamestate == 0 && jumping == false)
+	{
+		pikachuRunIndex++;
+		
+		if (pikachuRunIndex >= 4)
+			pikachuRunIndex = 0;
+	}
+	else if (gamestate == 0 && jumping == true)
+	{
+		pikachuJumpIndex++;
+		if (pikachuJumpIndex >= 5)
+			pikachuJumpIndex = 0;
+	}
+}
+
+void jump() {
+	if (jumping)
+	{
+		if (jumpingUp)
+		{
+			pikachuJumpCoordinate += 15;
+			if (pikachuJumpCoordinate > max_jump_limit)
+				jumpingUp = false;
+		}
+		else
+		{
+			pikachuJumpCoordinate -= 15;
+			if (pikachuJumpCoordinate <= 0)
+			{
+				jumping = false;
+				pikachuJumpCoordinate = 0;
+			}
+		}
+	}
+}
+
+void draw_movement() {
+	if (!jumping)
+	{
+		iShowBMP2(0, 0, pikachuRun[pikachuRunIndex], 0);
+	}
+	else
+	{
+		iShowBMP2(0, pikachuJumpCoordinate, pikachuJump[pikachuJumpIndex], 0);
+	}
+}
+
+void buttonWork() {
 	int sum = height - 500;
     for (int i = gamestate_limit-1; i >= 0; i--)
 	{
@@ -181,8 +240,12 @@ int main() {
 
     exitButton.x = 50;
     exitButton.y = height - 650;
+}
 
-    //iSetTimer(25, change);
+int main() {
+	buttonWork();
+    iSetTimer(100, index);
+	iSetTimer(30, jump);
 
     if (musicOn)
         PlaySound("music\\pokemon.wav", NULL, SND_LOOP | SND_ASYNC);
